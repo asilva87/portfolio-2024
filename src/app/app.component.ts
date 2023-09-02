9a2eecaaf2b99db42d65c82f914751a63ee08587
André Silva <dev01766@proton.me>
2023-09-03 00:08:14 +0200

    Set translation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GenerateProbabilityService } from './services/generate-probability/generate-probability.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'root',
@@ -9,9 +10,14 @@ import { GenerateProbabilityService } from './services/generate-probability/gene
 export class AppComponent {
     public splashScreenVersion!: number
 
-    constructor (private generateProbabiltyService: GenerateProbabilityService) {
+    constructor (private generateProbabiltyService: GenerateProbabilityService, private translateService: TranslateService) {
         this.splashScreenVersion = generateProbabiltyService.getRandomNumber()
 
-        console.log(this.splashScreenVersion)
+        // Tells the service which languages are available to use for translations.
+        translateService.addLangs(['en', 'de', 'ru'])
+        // Specify a fallback set of translations to use in case there are missing translations for the current language.
+        translateService.setDefaultLang('en')
+        // Tells the service which is the current language to use for translations.
+        translateService.use('de')
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SplashLetterComponent } from './pages/splash-screens/splash-letter/splash-letter.component';
-import { SplashBioComponent } from './pages/splash-screens/splash-bio/splash-bio/splash-bio.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { SplashLetterComponent } from './pages/splash-screens/splash-letter/splash-letter.component'
+import { SplashBioComponent } from './pages/splash-screens/splash-bio/splash-bio/splash-bio.component'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  }
 
 @NgModule({
     declarations: [
@@ -14,7 +22,16 @@ import { SplashBioComponent } from './pages/splash-screens/splash-bio/splash-bio
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'de',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent]
