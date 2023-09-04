ee4e153522682ae646048be6e7b3327c18082184
André Silva <dev01766@proton.me>
2023-09-04 10:42:44 +0200

    Add randomness for splash screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GenerateProbabilityService } from './services/generate-probability/generate-probability.service';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -7,17 +7,19 @@ import { TranslateService } from '@ngx-translate/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
     public splashScreenVersion!: number
 
-    constructor (private generateProbabiltyService: GenerateProbabilityService, private translateService: TranslateService) {
-        this.splashScreenVersion = generateProbabiltyService.getRandomNumber()
+    constructor (private generateProbabiltyService: GenerateProbabilityService, private translateService: TranslateService) { }
+
+    ngOnInit(): void {
+        this.splashScreenVersion = this.generateProbabiltyService.getRandomNumber(2)
 
         // Tells the service which languages are available to use for translations.
-        translateService.addLangs(['en', 'de', 'ru'])
+        this.translateService.addLangs(['en', 'de', 'ru'])
         // Specify a fallback set of translations to use in case there are missing translations for the current language.
-        translateService.setDefaultLang('en')
+        this.translateService.setDefaultLang('de')
         // Tells the service which is the current language to use for translations.
-        translateService.use('de')
+        this.translateService.use('en')
     }
 }
