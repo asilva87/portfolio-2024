67f9db912619fd90fc24b3058b048f9732c01d58
André Silva <dev01766@proton.me>
2024-01-22 16:45:49 +0100

    Add "NotFound" page route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Headache } from './pages/Headache'
+import { NotFound } from './pages/NotFound'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/headache" element={<Headache />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
   )
