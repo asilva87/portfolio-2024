7045895a2be91efadbf36e9eb1fd1105f1a372ec
André Silva <dev01766@proton.me>
2024-01-22 15:59:57 +0100

    Create and style "Headache" page

--- src/pages/Headache.tsx
+++ src/pages/Headache.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+import './Headache.css'
+
 export function Headache() {
     return (
-        <div>headache</div>
+        <div className="headache__wrapper">
+            <div className="headache__circle"></div>
+            <div className="headache__msg">
+                <p>Oh! You found this page! Go back! It's gonna strain your eyes!</p>
+            </div>
+            <Link to="/">
+                <div className="headache__goback">
+                    Take me outta here!
+                </div>
+            </Link>
+        </div>
     )
 }
