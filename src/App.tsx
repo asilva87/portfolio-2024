f0784a4ad507fac3a8e8ec789e41e5d749fb0a96
André Silva <dev01766@proton.me>
2024-01-23 12:57:16 +0100

    Add "About" page to router

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Headache } from './pages/Headache'
 import { NotFound } from './pages/NotFound'
+import { About } from './pages/About'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/headache" element={<Headache />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
